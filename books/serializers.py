686bbd06e25b38c16a7e32b1b7fc826c83d11df0
Gianik <paoduguran@gmail.com>
2022-11-09 21:52:26 +0800

    home page book list minor design

--- books/serializers.py
+++ books/serializers.py
@@ -16,20 +16,20 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class BookListSerializer(serializers.ModelSerializer):
 
-    type = serializers.CharField(source='get_type_display')
-    location = serializers.CharField(source='get_location_display')
-    status = serializers.CharField(source='get_status_display')
+    # type = serializers.CharField(source='get_type_display')
+    # location = serializers.CharField(source='get_location_display')
+    # status = serializers.CharField(source='get_status_display')
     author = AuthorSerializer(read_only=True, many=True)
 
     class Meta:
         model = Books
-        fields = '__all__'
+        fields = ['title', 'author']
 
-    def to_representation(self, instance):
-        response = super().to_representation(instance)
-        response['owner'] = instance.owner.first_name + \
-            ' ' + instance.owner.last_name
-        response['checked_out_by'] = instance.checked_out_by.first_name + \
-            ' ' + instance.checked_out_by.last_name
+    # def to_representation(self, instance):
+    #     response = super().to_representation(instance)
+    #     response['owner'] = instance.owner.first_name + \
+    #         ' ' + instance.owner.last_name
+    #     response['checked_out_by'] = instance.checked_out_by.first_name + \
+    #         ' ' + instance.checked_out_by.last_name
 
-        return response
+    #     return response
