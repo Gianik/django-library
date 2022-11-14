44d2dd6548feaa6b08d4b80b8046c442c9ef9038
Gianik <paoduguran@gmail.com>
2022-11-14 23:38:52 +0800

    homeList test for use in an external js file

---
+++ static/js/main.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+
+
+
+})
+function homeList() {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/book',
+        type: "GET",
+        dataType: "json",
+        success: (data) => {
+            var html = '';
+            $(data).each(function (index, value) {
+                html += "<div class=" + 'article-metadata' + " ><h2><a class=" + 'article-title' + " href=" + 'http://127.0.0.1:8000/book/detail/' + value.id + " >Title: " + value.title + "</a></h2>Author: "
+                $(value.author).each(function (index, value2) {
+                    html += "<a class=" + 'mr-2' + "  >" + value2.full_name + " </a>"
+                })
+                html += "</div>"
+            })
+
+            $("#div1").append(html)
+
+        },
+        error: (error) => {
+            console.log(error);
+        }
+    });
+};
